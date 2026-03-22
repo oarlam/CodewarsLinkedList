269cf23caf82419903e5a958bb51c0b83bfef5ab
oarlam <arlamovskao@gmail.com>
2026-03-22 13:49:58 +0200

    add task2 solution

---
+++ Parse a linked list from a string/task2.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(list_repr: str) -> Node | None:
+    list_repr = list_repr.replace('-> None', '')
+    list_repr = list_repr.replace('None', '')
+    if not list_repr:
+        return None
+    list_repr = list_repr.split(' -> ')
+    cur = Node(int(list_repr.pop(0)))
+    start = cur
+    for value in list_repr:
+        next_n = Node(int(value))
+        cur.next = next_n
+        cur = next_n
+    return start
