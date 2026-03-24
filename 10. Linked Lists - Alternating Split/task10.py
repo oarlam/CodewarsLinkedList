b484bc68432abc8246dccad92cb7eab6f8269149
oarlam <arlamovskao@gmail.com>
2026-03-24 14:10:21 +0200

    add task 10 solution

---
+++ 10. Linked Lists - Alternating Split/task10.py
@@ -0,0 +1,26 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+def alternating_split(head):
+    head2 = head.next
+    node1 = head
+    if node1.next:
+        node2 = node1.next
+    else:
+        raise ValueError('splitting a single node')
+    while node1 and node2:
+        node1.next = node2.next
+        try:
+            node2.next = node2.next.next
+        except AttributeError:
+            node2.next = None
+        node1 = node1.next
+        node2 = node2.next
+    return Context(head, head2)
